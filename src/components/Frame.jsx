28b9926fa46ee10afe87419e7feede16fb72821e
Hugo Florentino <hugoflorentino86@hotmail.com>
2025-03-27 16:25:50 +0000

    Fixing the frame bug

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,6 +1,6 @@
 const Frame = () => {
   return (
-    <div className='fixed top-0 left-0 right-0 bottom-0 border-4 rounded-md border-blue-500 shadow-[0_0_10px_rgba(0,150,255,0.6)] pointer-events-none  z-50' />
+    <div className='fixed top-0 left-0 right-0 bottom-0 h-full border-4 rounded-md border-blue-500 shadow-[0_0_10px_rgba(0,150,255,0.6)] pointer-events-none  z-50' />
   );
 };
 
